99d01f6b133d0ff210e629f6a671edb83b8074e1
Domegal13 <Domegal13cursos@gmail.com>
2024-04-30 23:51:12 -0300

    se agrego transition al navbar

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ const navList = document.querySelector(".nav-list");
 const closeNav = document.querySelector(".close-nav");
 
 menuHmab.addEventListener("click", () => {
-  navList.classList.toggle("hidden");
+  // navList.classList.toggle("hidden");
+  navList.classList.toggle("translate-y-[1px]");
 });
 
 closeNav.addEventListener("click", () => {
-  navList.classList.add("hidden");
+  // navList.classList.add("hidden");
+  navList.classList.remove("translate-y-[1px]");
 });
