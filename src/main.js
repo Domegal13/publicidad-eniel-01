375d5feb63eccb8a2862c97aa1c46c805a750a3e
Domegal13 <Domegal13cursos@gmail.com>
2024-04-30 23:21:32 -0300

    Actualizacion del navbar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const menuHmab = document.querySelector(".menu-hamb");
 const navList = document.querySelector(".nav-list");
+const closeNav = document.querySelector(".close-nav");
 
 menuHmab.addEventListener("click", () => {
   navList.classList.toggle("hidden");
+});
+
+closeNav.addEventListener("click", () => {
+  navList.classList.add("hidden");
 });
